23cb9cec98b7cccdc435db8bc00eef8704780c57
sg-kumaraguru <sg.kumaraguru@gmail.com>
2026-04-26 12:14:06 +0530

    chore: add helmet middleware for HTTP header protection

--- app/app.js
+++ app/app.js
@@ -1,11 +1,16 @@
-import express from "express";
+import express from "express"
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+
 import {errorHandler} from "./middlewares/errorMiddleware.js"
 import authRouter from "./routes/authRoutes.js";
 import blogPostRouter from "./routes/blogPostRoutes.js";
 import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
+
+app.use(helmet());
+app.disable("x-powered-by");
 
 app.use(cookieParser());
 app.use(express.json());
